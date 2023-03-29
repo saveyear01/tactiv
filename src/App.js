02fff7b4e50a165f0601712e11b0e86747021f0e
James Joshua Ginete <jjginete@dev.empowerwealth.com.au>
2023-03-29 10:52:58 +0800

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +19,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer>
+        <ul>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+        </ul>
+      </footer>
     </div>
   );
 }
